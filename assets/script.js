0e09cdef1efd005fd8ef5cabad3885e86322ae05
Theresa E <t.eatherly@gmail.com>
2020-03-17 12:35:03 -0500

    3 API URLs added

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,31 @@
 // Required in objects:
 //    * UV Index (with color representation)  
-//    _Hint: Multiple API endpoints will be required._
     // Local Storage Set
 
 // set Variables
 var apiKey = "a40d2c347523bc6c683485054db1e19c";
-var queryURLBase = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" + apiKey;
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=a40d2c347523bc6c683485054db1e19c";
+// var queryURLBase = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" + apiKey;
+
+var queryURLFiveDay = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=a40d2c347523bc6c683485054db1e19c";
 console.log(queryURLBase);
- 
+// var queryURLToday = "api.openweathermap.org/data/2.5/weather?q={city name}&APPID=" + apiKey 
+// var uvURLBase = "http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}";
+// console.log(uvURLBase);
+var lat = 36.162; 
+console.log(lat);
+var lon =-86.78;
+// assign inside ajax URL today (response.lat.)
+ var uvURL = "http://api.openweathermap.org/data/2.5/uvi?APPID=" + apiKey +  "&lat=" + lat + "&lon=" +lon;
+console.log(uvURL);
 
 
+var queryTerm = localStorage.getItem("searchTerm") || "Nashville";
 
-var queryTerm = [];
-// $("queryTerm").textContent = userInput;
-// localStorage.setItem("#searchTerm" , userInput);
-// localStorage.getItem("#searchTerm" , queryTermList)=$("<div>");
-// var userInput = $("queryTerm-input").val();;
-// console.log(userInput);
+var queryTermList = JSON.parse(localStorage.getItem("searchTermList")) || ["Nashville"];
 
 var queryTerm=$("#searchTerm").val();;
-queryTermList = JSON.parse(localStorage.getItem("queryTerm")) || [];
-localStorage.getItem(queryTerm, queryTermList);
+
+
 console.log(queryTermList);
 $("#history").append(queryTerm);
   function lastCitySearch() {
@@ -39,10 +43,7 @@ $("#history").append(queryTerm);
 var now = moment().format("LLLL");
   // console.log(now);
   $("#currentTime").append(now);
-// Five Day Forecast
-// for (var i = 0; i < response.list.length; i++){
-    // console.log (response.list.length);
-    //  temperature id=currentWeather
+
 
 var queryTerm ="";
 function runQuery(queryURL){
@@ -59,12 +60,23 @@ function runQuery(queryURL){
     console.log(response.list[0].main.humidity);
     $("#humidity").text(response.list[0].main.humidity);
     $("#currentHumidity").append("Humidity: " + response.list[0].main.humidity + "  %");
-    // $("#currentWeather").append(humidity);
+   
     // console.log(response.list[i].main.weather.icon);
+
     console.log(response.list[0].wind.speed);
     $("#wind").text(response.list[0].wind.speed);
     $("#currentWind").append("Wind: @ " + response.list[0].wind.speed  +  "mph");
+})
+}
 
+
+
+
+
+// Five Day Forecast
+// for (var i = 0; i < response.list.length; i++){
+    // console.log (response.list.length);
+    //  temperature id=currentWeather
 // console.log(response.list[7].main.temp);
 // localStorage.setItem("currentTemp", response.list[7].main.temp);
 // localStorage.setItem("currentTempList", JSON.stringify([response.list[7].main.temp]));
@@ -73,35 +85,47 @@ function runQuery(queryURL){
 // var currentTempList = JSON.parse(localStorage.getItem("currentTempList")) || [];
 // console.log(currentTemp, currentTempList);
 // //  grab id to chang HTML response.main.temp / response.main.uv
+// var tomorrowSection = $("<div>");
+// $("#tomorrowSection").attr("class - card");
+// // <!-- <img src="..." class="card-img-top" alt="..."> -->
+// var tomorrow = $("#<div>");
+// $("#tomorrow").attr("class - card-body");
+//   <h5 class="card-title">Tomorrow</h5>
+//   <p class="card-text" id="dayTemp"></p>
+//   <p class="card-text" id="dayHumidity"></p>
+//   <p class="card-text" id="dayWind"></p>
+//   <p class="card-text" id="dayUvIndex"></p>
 
-// // set Functions
+  
 
-    // }
-  })
-}
 
 // Main Process to run Query
 $("#searchBtn").on("click", function(event){
   event.preventDefault();
-  var queryTermList = localStorage.setItem("#searchTerm", JSON.stringify([queryTerm])) || "Nashville";
-  var queryTerm = [];
-  var queryTerm=$("#searchTerm").val();;
-  localStorage.setItem("#searchTerm", queryTerm);
-localStorage.setItem("searchTermList", queryTermList);
+   var userInput=$("#searchTerm").val();
+   queryTerm = userInput;
+  queryTermList.push(userInput);
+ 
+  localStorage.setItem("searchTerm", queryTerm);
+localStorage.setItem("searchTermList", JSON.stringify(queryTermList));
 
 
 //  queryTermList = JSON.parse(localStorage.setItem("queryTerm")) || [];
 
   var queryTerm = $("#searchTerm").val().trim();
+  $("#currentCity").empty();
   $("#currentCity").append(queryTerm);
+
   var newURL = queryURLBase + "&q=" + queryTerm;
   runQuery(newURL);
-  // runQuery("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" + apiKey);
-  // alert("hell0");
   console.log(queryTerm);
   console.log(newURL);
  
 });
 
 
-
+// new search query for each API?
+// get icons
+// work on local storage appending
+// API for UV
+// same API for 5 day?
