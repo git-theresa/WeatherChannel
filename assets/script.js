d3431e9f5c8f26f64e7cdc3245f89a51b20dfe05
Theresa E <t.eatherly@gmail.com>
2020-03-19 16:27:15 -0500

    all five day cards added

--- assets/script.js
+++ assets/script.js
@@ -47,14 +47,14 @@ function runQuery(){
     $.ajax({url: queryURLToday + queryTerm, method: "GET"})
     .then(function(response) {
     console.log(response);
-   $("#currentTemp").empty();
-
-$("#currentWind").empty();
-$("#currentHumidity").empty();
+    $("#currentTemp").empty();
+    $("#currentWind").empty();
+    $("#currentHumidity").empty();
     var lat = response.coord.lat;
     var lon = response.coord.lon;
-    var uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey +  "&lat=" + lat + "&lon=" + lon;
-   
+    var uvURL = "https://api.openweathermap.org/data/2.5/uvi?q=" + queryTerm + "&appid=" + apiKey +  "&lat=" + lat + "&lon=" + lon;
+    // "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial"
+
     var temp = $("<div>");
     
     $("#temp").text(response.main.temp);
@@ -68,90 +68,55 @@ $("#currentHumidity").empty();
 
     console.log(response.main.humidity);
     $("#humidity").text(response.main.humidity);
-    
     $("#currentHumidity").append("Humidity: " + response.main.humidity + "  %");
-   
     console.log(response.wind.speed);
     $("#wind").text(response.wind.speed);
     $("#currentWind").append("Wind: @ " + response.wind.speed  +  "mph");
-    
-
+      // UV Index
       $.ajax({url: uvURL, method: "GET"})
       .then(function(resp) {
       console.log(resp);
        $("#uv").empty();
       $("#uv").append("UV Index: " + resp.value);
-     
-     })
+      })
     })
+      // FIVE DAY FORECAST FUNCTION
+      var queryURLFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + queryTerm +"&appid=" + apiKey;    
+      $.ajax({url: queryURLFiveDay, method: "GET"})
+      .then(function(forecastData) {
+      console.log(forecastData)
+      
+      $("#fiveDay").empty();
+      $("#fiveDayTemp").empty();
+      $("#fiveDayHumidity").empty();
+      $("#fiveDayWind").empty();
 
-    // Five DAY 
-    $.ajax({url: queryURLFiveDay + queryTerm, method: "GET"})
-    .then(function(response) {
      for (var i = 0; i < 5; i++) {
-    // console.log (response.list.length);
-    var temp = $("<div>");
-    $("#temp").text(response.list[0].main.temp);
-    // var currentTemp= (response.list[0].main.temp - 273.15) * 1.8 + 32;
-    console.log(currentTemp);
-    // $("#currentTemp").append("Temperature: "+     currentTemp.toFixed() + " F");
-    // // console.log(response.list[0].main.temp);
-    }
-  });
-  // console.log(response.list[0].main.humidity);
-  // $("#humidity").text(response.list[0].main.humidity);
-  // $("#currentHumidity").append("Humidity: " + response.list[0].main.humidity + "  %");
-
-  // console.log(response.list[0].wind.speed);
-  // $("#wind").text(response.list[0].wind.speed);
-  // $("#currentWind").append("Wind: @ " + response.list[0].wind.speed  +  "mph");
-   
-
-  // $("#tomorrow").attr("class - card-body");
-  //   <h5 class="card-title">Tomorrow</h5>
-  //   <p class="card-text" id="dayTemp"></p>
-  //   <p class="card-text" id="dayHumidity"></p>
-  //   <p class="card-text" id="dayWind"></p>
-  //   <p class="card-text" id="dayUvIndex"></p>
-
-
  
+      var fiveDayFaren = Math.floor((forecastData.list[i].main.temp- 273.15) * 1.8 + 32);
+      var fiveDayCard = $("<div class='card'>");
+       var fiveDayTemp = $("<div>");
+       fiveDayTemp.text(fiveDayFaren);
+        fiveDayCard.append("Temp: " + fiveDayFaren + " F");
+       $("#fiveDay").append(fiveDayCard);
+      
+      var fiveDayHumidity = $("<div>");
+      fiveDayHumidity.text(forecastData.list[i].main.humidity);
+      fiveDayCard.append("Humidity: " + forecastData.list[i].main.humidity + "%");
+      $("#fiveDay").append(fiveDayCard);
+       
+      var fiveDayWind = $("<div>");
+      fiveDayWind.text(forecastData.list[i].wind.speed);
+      fiveDayCard.append("Wind @ " + forecastData.list[i].wind.speed + "mph");
+      $("fiveDay").append(fiveDayCard);
+ 
+
+      }
+  });
+ 
+
 }
 
-  
- 
-
-
-
-
- 
- 
-
-
-
-
-
-
-// //  grab id to chang HTML response.main.temp / response.main.uv
-// var tomorrowSection = $("<div>");
-// $("#tomorrowSection").attr("class - card");
-// // <!-- <img src="..." class="card-img-top" alt="..."> -->
-// var tomorrow = $("#<div>");
-  // var lat = 36.162; 
-// console.log(lat);
-// var lon =-86.78;
-  
-// $.ajax({url: uvURL, method: "GET"})
-// .then(function(response){
-// $("#uv").text(response.value);
-// console.log(uvURL)
-// var uvURLBase ="http://api.openweathermap.org/data/2.5/uvi?appid={a40d2c347523bc6c683485054db1e19c}&lat={lat}&lon={lon}"
-// var lat = response.coord.lat;
-//   var lon = response.coord.lon;
-//  var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey +  "&lat=" + lat + "&lon=" +lon;
-
-
-  
 
 
 // Main Process to run Query
